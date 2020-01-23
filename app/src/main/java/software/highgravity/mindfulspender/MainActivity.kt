e7ad00bf3f93e1a2393ee93255528e42f14d53e2
BillyG <bgliddenjr@gmail.com>
2020-01-23 16:04:54 -0500

    Initial commit

---
+++ app/src/main/java/software/highgravity/mindfulspender/MainActivity.kt
@@ -0,0 +1,12 @@
+package software.highgravity.mindfulspender
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
